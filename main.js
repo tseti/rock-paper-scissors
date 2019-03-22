8982e1f1132a87d0f0d008c17e1499ed89885316
tseti <fred_luo@msn.com>
2019-03-22 18:53:32 +0100

    Finish play and computerPlay functions

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
 "use strict"
 
+function computerPlay() {
+  let choices = ['Rock', 'Paper', 'Scissors'];
+
+  return choices[Math.trunc(Math.random() * 3)];
+}
+
+function play(playerSelection, computerSelection) {
+  let [player, cpu] = [playerSelection.toLowerCase(), computerSelection.toLowerCase()];
+  let result = (choice) => cpu === choice 
+             ? `You Win! ${player} beats ${cpu}` 
+             : `You Lose! ${cpu} beats ${player}`;
+
+  switch (player) {
+    case (cpu):         return `Draw! You both chose ${player}`;
+    case ('rock'):      return result('scissors');
+    case ('paper'):     return result('rock');
+    case ('scissors'):  return result('paper');
+    default:            return 'Invalid input!';
+  }
+}
